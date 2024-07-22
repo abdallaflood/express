19ae8bfdcae43934a7b70a8eb5c0c730be34d298
abdallaflood <wtuewtu@gmail.com>
2024-07-22 03:44:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb+srv://devabdalla:Qdsrlu3m1GR3Etuh@cluster0.3dlmk4a.mongodb.net/').then( () =>{
+  console.log("database")
+  app.get('/', (req, res) => {
+  res.sendFile("./views/home.html",{root: __dirname})
+})
+}).catch((arr) => {console.log(err)});
+
+app.listen(port, () => {
+  console.log(`http://localhost:$(port)/`)
+})
